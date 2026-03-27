ff29af1160ef32799a7393ad4b965df8cf2263e5
vaghelaharsh1015 <harshvaghela4277@gmail.com>
2026-03-27 14:34:14 +0530

    new file

---
+++ day_2/nestedcomponets/apolo/src/components/card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div>
+      <div>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/4_cilindros_de_BMW%2C_M%C3%BAnich%2C_Alemania1.jpg/250px-4_cilindros_de_BMW%2C_M%C3%BAnich%2C_Alemania1.jpg"
+          alt=""
+        />
+      </div>
+      <h1>B.M.W</h1>
+    </div>
+  );
+};
+
+export default card;
